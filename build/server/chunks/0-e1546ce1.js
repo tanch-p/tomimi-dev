c1381dc31b58a971b4b3aa671c5ebc4c9f7dc93f
Chor <97508567+tanch-p@users.noreply.github.com>
2024-04-04 15:44:37 +0800

    :wqchanged adapter to node

---
+++ build/server/chunks/0-e1546ce1.js
@@ -0,0 +1,9 @@
+const index = 0;
+let component_cache;
+const component = async () => component_cache ??= (await import('./_layout.svelte-11e4e4d2.js')).default;
+const imports = ["_app/immutable/nodes/0.907ac265.js","_app/immutable/chunks/index.761c3231.js","_app/immutable/chunks/Icon.6c7978e4.js","_app/immutable/chunks/stores.628437be.js","_app/immutable/chunks/singletons.4c024dbe.js"];
+const stylesheets = ["_app/immutable/assets/0.c11d61ec.css"];
+const fonts = [];
+
+export { component, fonts, imports, index, stylesheets };
+//# sourceMappingURL=0-e1546ce1.js.map
