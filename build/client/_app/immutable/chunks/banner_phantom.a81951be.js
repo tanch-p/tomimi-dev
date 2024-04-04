7da6f54e4220ac1da26aff1583e110db7d0408f6
Chor <97508567+tanch-p@users.noreply.github.com>
2024-04-04 15:44:37 +0800

    :wqchanged adapter to node

---
+++ build/client/_app/immutable/chunks/banner_phantom.a81951be.js
@@ -0,0 +1,1 @@
+const e=""+new URL("../assets/banner_phantom.cb35fc7c.webp",import.meta.url).href;export{e as b};
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { pagesToRender } from './src/lib/pagesList.js';
 
