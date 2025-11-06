348bc51b6908f34608ab62146183d1e9e4343969
Chor <97508567+tanch-p@users.noreply.github.com>
2025-11-06 19:57:02 +0900

    add sbell2_icetgt to tokensWithoutIcon

--- src/routes/[lang=lang]/(app)/recruit/CharaTokens.svelte
+++ src/routes/[lang=lang]/(app)/recruit/CharaTokens.svelte
@@ -18,7 +18,8 @@
 	const tokensWithoutIcon = [
 		'token_10012_rosmon_shield',
 		'token_10015_dusk_drgn',
-		'token_10040_siege2_vlion'
+		'token_10040_siege2_vlion',
+		'token_10058_sbell2_icetgt'
 	];
 	const statKeys = ['hp', 'respawnTime', 'atk', 'cost', 'def', 'blockCnt', 'res', 'aspd'];
 
